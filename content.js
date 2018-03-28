89953f61c51b4fb4c7c940f01308550f2103d0cb
Manuel <manuel.torre@aero.tur.ar>
2018-03-27 21:38:49 -0300

    first commit

---
+++ content.js
@@ -0,0 +1,32 @@
+document.onreadystatechange = function () {
+  const bla = document.querySelectorAll("h1, h2, h3")
+  function getPathTo(element) {
+      if (element.id!=='')
+          return 'id("'+element.id+'")';
+      if (element===document.body)
+          return element.tagName;
+
+      var ix= 0;
+      var siblings= element.parentNode.childNodes;
+      for (var i= 0; i<siblings.length; i++) {
+          var sibling= siblings[i];
+          if (sibling===element)
+              return getPathTo(element.parentNode)+'/'+element.tagName+'['+(ix+1)+']';
+          if (sibling.nodeType===1 && sibling.tagName===element.tagName)
+              ix++;
+      }
+  }
+  Array.prototype.forEach.call(bla, function(item){
+    item.style.backgroundColor = "red";
+    item.setAttribute('draggable', true);
+    item.ondragstart = function(e) {
+      e.dataTransfer.setData("text", getPathTo(e.target))
+    };
+  })
+}
+// window.onload = function() {
+//     var iframe = document.createElement('iframe');
+//     iframe.src = "https://twitter.com";
+//     iframe.style.position = "absolute";
+//     document.body.appendChild(iframe);
+// };
---
+++ showIframe.js
@@ -0,0 +1,47 @@
+
+if (!document.getElementById("iframe-extension")) {
+  var iframe = document.createElement('iframe');
+  iframe.src = "http://localhost:3000";
+  iframe.sandbox = "allow-scripts";
+  iframe.style.position = "fixed";
+  iframe.style.right = "50px";
+  iframe.style.top = "50px";
+  iframe.style.height = "500px";
+  iframe.id = "iframe-extension";
+  document.body.appendChild(iframe);
+
+  var mask = document.createElement('div');
+  mask.style.position = "fixed";
+  mask.style.right = "50px";
+  mask.style.top = "50px";
+  mask.style.height = "500px";
+  mask.style.width = "300px";
+  mask.id = "iframe-mask";
+  mask.background = "transparent"
+  document.body.appendChild(mask);
+
+  var getElementByXpath = function(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  }
+  // mask.ondragstart = function(e) {
+  //   console.log(e.target.id)
+  //   e.dataTransfer.setData("text", e.target.id);
+  // };
+  mask.ondragover = function(e){
+    e.preventDefault()
+  };
+
+  mask.ondrop = function(e){
+    var elemento = e.dataTransfer.getData("text");
+    console.log(getElementByXpath('//' + elemento))
+  };
+
+}
+else{
+  if (document.getElementById("iframe-extension").style.visibility == "hidden") {
+    document.getElementById("iframe-extension").style.visibility = "visible"
+  }
+  else{
+    document.getElementById("iframe-extension").style.visibility = "hidden"
+  }
+}
