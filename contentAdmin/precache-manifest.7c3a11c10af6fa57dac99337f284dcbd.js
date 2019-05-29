8d78751114e71765c88625107cbe38b1d0b241aa
Manuel <manuel.torre@aero.tur.ar>
2019-05-29 00:44:17 -0300

    last version of boths apps

---
+++ contentAdmin/precache-manifest.7c3a11c10af6fa57dac99337f284dcbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "db3a2b95425caf75adf4",
+    "url": "./static/js/main.db3a2b95.chunk.js"
+  },
+  {
+    "revision": "0536b2eb814cecb1d838",
+    "url": "./static/js/1.0536b2eb.chunk.js"
+  },
+  {
+    "revision": "db3a2b95425caf75adf4",
+    "url": "./static/css/main.2695aed6.chunk.css"
+  },
+  {
+    "revision": "0536b2eb814cecb1d838",
+    "url": "./static/css/1.c8d035f8.chunk.css"
+  },
+  {
+    "revision": "f4573494dc16ce0994ad4793bf759f0b",
+    "url": "./index.html"
+  }
+];
